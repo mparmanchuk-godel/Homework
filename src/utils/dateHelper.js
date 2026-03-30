52c82af9a6ddac10c5e519b65998cce9da4f017f
mparmanchuk-godel <m.parmanchuk@godeltech.com>
2026-03-30 10:01:14 +0200

    SDET Part 2 Ex 5 step 2

--- src/utils/dateHelper.js
+++ src/utils/dateHelper.js
@@ -1,6 +1,7 @@
 // path: src/utils/dateHelper.js
 
 function formatDate(date) {
+  if (date == null) return "";
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, '0');
   const day = String(date.getDate()).padStart(2, '0');
